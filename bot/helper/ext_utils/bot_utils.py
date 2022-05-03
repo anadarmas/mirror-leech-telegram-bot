c46223446648335429df6aa9c22a4c67cce15e91
Ana de Armas <101505047+anadarmas@users.noreply.github.com>
2022-05-03 06:26:01 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -1,23 +1,16 @@
-import time
-import math
-
-import shutil
-import psutil
 from re import match, findall
 from threading import Thread, Event
+from time import time
+from math import ceil
+from html import escape
 from psutil import virtual_memory, cpu_percent, disk_usage
 from requests import head as rhead
 from urllib.request import urlopen
-from telegram import InlineKeyboardMarkup, InlineKeyboardButton
-from telegram.ext import CallbackContext, CallbackQueryHandler
-from telegram.message import Message
-from telegram.update import Update
+from telegram import InlineKeyboardMarkup
 
 from bot.helper.telegram_helper.bot_commands import BotCommands
-from bot import dispatcher, download_dict, download_dict_lock, STATUS_LIMIT, botStartTime
+from bot import download_dict, download_dict_lock, STATUS_LIMIT, botStartTime, DOWNLOAD_DIR
 from bot.helper.telegram_helper.button_build import ButtonMaker
-from bot.helper.telegram_helper import message_utils
-from bot.helper.telegram_helper.message_utils import *
 
 MAGNET_REGEX = r"magnet:\?xt=urn:btih:[a-zA-Z0-9]*"
 
@@ -26,8 +19,6 @@ URL_REGEX = r"(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+"
 COUNT = 0
 PAGE_NO = 1
 
-FINISHED_PROGRESS_STR = "█"
-UNFINISHED_PROGRESS_STR = "░"
 
 class MirrorStatus:
     STATUS_UPLOADING = "Uploading...📤"
@@ -43,7 +34,7 @@ class MirrorStatus:
     STATUS_SEEDING = "Seeding...🌧"
 
 SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
-PROGRESS_MAX_SIZE = 100 // 8
+
 
 class setInterval:
     def __init__(self, interval, action):
@@ -107,143 +98,18 @@ def getAllDownload(req_status: str):
                     return dl
                 elif req_status == 'all':
                     return dl
-    return False
+    return None
 
 def get_progress_bar_string(status):
     completed = status.processed_bytes() / 8
     total = status.size_raw() / 8
-    if total == 0:
-        p = 0
-    else:
-        p = round(completed * 100 / total)
+    p = 0 if total == 0 else round(completed * 100 / total)
     p = min(max(p, 0), 100)
     cFull = p // 8
-    cPart = p % 8 - 1
-    p_str = FINISHED_PROGRESS_STR * cFull
-    if cPart >= 0:
-        p_str += FINISHED_PROGRESS_STR
-    p_str += UNFINISHED_PROGRESS_STR * (PROGRESS_MAX_SIZE - cFull)
+    p_str = '■' * cFull
+    p_str += '□' * (12 - cFull)
     p_str = f"[{p_str}]"
     return p_str
-
-
-def progress_bar(percentage):
-    """Returns a progress bar for download
-    """
-    #percentage is on the scale of 0-1
-    comp = FINISHED_PROGRESS_STR
-    ncomp = UNFINISHED_PROGRESS_STR
-    pr = ""
-
-    if isinstance(percentage, str):
-        return "NaN"
-
-    try:
-        percentage=int(percentage)
-    except:
-        percentage = 0
-
-    for i in range(1,11):
-        if i <= int(percentage/10):
-            pr += comp
-        else:
-            pr += ncomp
-    return pr
-
-def sendMessage(text: str, bot, update: Update):
-    try:
-        return bot.send_message(update.message.chat_id,
-                            reply_to_message_id=update.message.message_id,
-                            text=text, allow_sending_without_reply=True, parse_mode='HTMl', disable_web_page_preview=True)
-    except RetryAfter as r:
-        LOGGER.warning(str(r))
-        time.sleep(r.retry_after * 1.5)
-        return sendMessage(text, bot, update)
-    except Exception as e:
-        LOGGER.error(str(e))
-        return
-
-def sendMarkup(text: str, bot, update: Update, reply_markup: InlineKeyboardMarkup):
-    try:
-        return bot.send_message(update.message.chat_id,
-                            reply_to_message_id=update.message.message_id,
-                            text=text, reply_markup=reply_markup, allow_sending_without_reply=True,
-                            parse_mode='HTMl', disable_web_page_preview=True)
-    except RetryAfter as r:
-        LOGGER.error(str(r))
-        time.sleep(r.retry_after)
-        return sendMarkup(text, bot, update, reply_markup)
-    except Exception as e:
-        LOGGER.error(str(e))
-
-def editMessage(text: str, message: Message, reply_markup=None):
-    try:
-        bot.edit_message_text(text=text, message_id=message.message_id,
-                              chat_id=message.chat.id,reply_markup=reply_markup,
-                              parse_mode='HTMl', disable_web_page_preview=True)
-    except RetryAfter as r:
-        LOGGER.warning(str(r))
-        time.sleep(r.retry_after * 1.5)
-        return editMessage(text, message, reply_markup)
-    except Exception as e:
-        LOGGER.error(str(e))
-        return
-
-def deleteMessage(bot, message: Message):
-    try:
-        bot.delete_message(chat_id=message.chat.id,
-                           message_id=message.message_id)
-    except Exception as e:
-        LOGGER.error(str(e))
-
-def auto_delete_message(bot, cmd_message: Message, bot_message: Message):
-    if AUTO_DELETE_MESSAGE_DURATION != -1:
-        time.sleep(AUTO_DELETE_MESSAGE_DURATION)
-        try:
-            # Skip if None is passed meaning we don't want to delete bot xor cmd message
-            deleteMessage(bot, cmd_message)
-            deleteMessage(bot, bot_message)
-        except AttributeError:
-            pass
-
-def delete_all_messages():
-    with status_reply_dict_lock:
-        for message in list(status_reply_dict.values()):
-            try:
-                deleteMessage(bot, message)
-                del status_reply_dict[message.chat.id]
-            except Exception as e:
-                LOGGER.error(str(e))
-
-def update_all_messages():
-    msg, buttons = get_readable_message()
-    with status_reply_dict_lock:
-        for chat_id in list(status_reply_dict.keys()):
-            if status_reply_dict[chat_id] and msg != status_reply_dict[chat_id].text:
-                if buttons == "":
-                    editMessage(msg, status_reply_dict[chat_id])
-                else:
-                    editMessage(msg, status_reply_dict[chat_id], buttons)
-                status_reply_dict[chat_id].text = msg
-
-def sendStatusMessage(msg, bot):
-    if len(Interval) == 0:
-        Interval.append(setInterval(DOWNLOAD_STATUS_UPDATE_INTERVAL, update_all_messages))
-    progress, buttons = get_readable_message()
-    with status_reply_dict_lock:
-        if msg.message.chat.id in list(status_reply_dict):
-            try:
-                message = status_reply_dict[msg.message.chat.id]
-                deleteMessage(bot, message)
-                del status_reply_dict[msg.message.chat.id]
-            except Exception as e:
-                LOGGER.error(str(e))
-                del status_reply_dict[msg.message.chat.id]
-        if buttons == "":
-            message = sendMessage(progress, bot, msg)
-        else:
-            message = sendMarkup(progress, bot, msg, buttons)
-        status_reply_dict[msg.message.chat.id] = message
 
 def get_readable_message():
     with download_dict_lock:
@@ -312,23 +178,14 @@ def get_readable_message():
                 elif 'MB/s' in spd:
                     upspeed_bytes += float(spd.split('M')[0]) * 1048576
         bmsg += f"\n<b>DL:</b> {get_readable_file_size(dlspeed_bytes)}/s | <b>UL:</b> {get_readable_file_size(upspeed_bytes)}/s"
-        buttons = ButtonMaker()
-        buttons.sbutton("Refresh", str(ONE))
-        buttons.sbutton("Stats", str(THREE))
-        buttons.sbutton("Close", str(TWO))
-        sbutton = InlineKeyboardMarkup(buttons.build_menu(2))
         if STATUS_LIMIT is not None and tasks > STATUS_LIMIT:
             msg += f"<b>Page:</b> {PAGE_NO}/{pages} | <b>Tasks:</b> {tasks}\n"
             buttons = ButtonMaker()
-            buttons.sbutton("Previous Page", "pre")
-            buttons.sbutton("Refresh", str(ONE))
-            buttons.sbutton("Next Page", "nex")
-            buttons.sbutton("Stats", str(THREE))
-            buttons.sbutton("Close", str(TWO))
-            button = InlineKeyboardMarkup(buttons.build_menu(3))
+            buttons.sbutton("Previous", "status pre")
+            buttons.sbutton("Next", "status nex")
+            button = InlineKeyboardMarkup(buttons.build_menu(2))
             return msg + bmsg, button
         return msg + bmsg, ""
-
 
 def turn(data):
     try:
@@ -412,61 +269,3 @@ def get_content_type(link: str) -> str:
         except:
             content_type = None
     return content_type
-
-ONE, TWO, THREE = range(3)
-
-def refresh(update, context):
-    query = update.callback_query
-    query.edit_message_text(text="Refreshing Status...⏳")
-    time.sleep(2)
-    update_all_messages()
-    
-def close(update, context):
-    chat_id  = update.effective_chat.id
-    user_id = update.callback_query.from_user.id
-    bot = context.bot
-    query = update.callback_query
-    admins = bot.get_chat_member(chat_id, user_id).status in ['creator', 'administrator'] or user_id in [OWNER_ID]
-    if admins:
-        delete_all_messages()
-    else:
-        query.answer(text="You Don't Have Admin Rights!", show_alert=True)
-        
-def pop_up_stats(update, context):
-    query = update.callback_query
-    stats = bot_sys_stats()
-    query.answer(text=stats, show_alert=True)
-
-def bot_sys_stats():
-    currentTime = get_readable_time(time.time() - botStartTime)
-    cpu = psutil.cpu_percent()
-    mem = psutil.virtual_memory().percent
-    disk = psutil.disk_usage("/").percent
-    total, used, free = shutil.disk_usage('.')
-    total = get_readable_file_size(total)
-    used = get_readable_file_size(used)
-    free = get_readable_file_size(free)
-    recv = get_readable_file_size(psutil.net_io_counters().bytes_recv)
-    sent = get_readable_file_size(psutil.net_io_counters().bytes_sent)
-    stats = f"""
-BOT UPTIME 🕐 : {currentTime}
-
-CPU : {progress_bar(cpu)} {cpu}%
-RAM : {progress_bar(mem)} {mem}%
-
-DISK : {progress_bar(disk)} {disk}%
-TOTAL : {total}
-
-USED : {used} || FREE : {free}
-SENT : {sent} || RECV : {recv}
-"""
-    return stats
-
-dispatcher.add_handler(CallbackQueryHandler(refresh, pattern='^' + str(ONE) + '$'))
-dispatcher.add_handler(CallbackQueryHandler(close, pattern='^' + str(TWO) + '$'))
-dispatcher.add_handler(CallbackQueryHandler(pop_up_stats, pattern='^' + str(THREE) + '$'))
-
-next_handler = CallbackQueryHandler(turn, pattern="nex", run_async=True)
-previous_handler = CallbackQueryHandler(turn, pattern="pre", run_async=True)
-dispatcher.add_handler(next_handler)
-dispatcher.add_handler(previous_handler)
